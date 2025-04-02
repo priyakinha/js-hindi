7a4b2aa3e57a9e15dd6814ed2fd102e4ff673baf
priyakinha <153503891+priyakinha@users.noreply.github.com>
2025-04-02 18:18:28 +0000

    dataTypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=134;
+let accountEmail="priya@google.com";
+var accountPassword="123";
+//accountId=566;
+console.table([accountId,accountEmail,accountPassword]);
+//datatypes
+// Int,bigint,null,undefined,string,boolean,symbol,objects
